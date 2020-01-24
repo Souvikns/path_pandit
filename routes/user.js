e6aaeb27af8d96001f857b9906a2f9d863e2002f
souvik <41781438+Souvikns@users.noreply.github.com>
2020-01-24 17:20:36 +0530

    order module making

--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,22 @@ const router = express.Router()
 
 //Routes 
 
+router.get('/order',(req,res)=>{
+    res.render('order',{
+        isLogin: req.session.isLogin
+    })
+})
+
+
+router.post('/order',(req,res)=>{
+    date = req.body.date 
+    time = req.body.time 
+    data = {
+        date: date,
+        time: time
+    }
+    res.send(data)
+})
 
 //=============================
 
