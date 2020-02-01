fe69eea9a1714ddf0d249c54bc0b61173dd9ec79
Souvikns <41781438+Souvikns@users.noreply.github.com>
2020-02-01 14:10:23 +0530

    pandit dashboard adder

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,36 +1,44 @@
 const Puja = require('../model/pooja')
 const details = require('../util/filehandling')
 
-exports.getHomePage = (req,res)=>{
-    Puja.findAll({}).then(data=>{
-        res.render('index',{
-            puja: data,
-            isLogin: req.session.isLogin
-        })
+exports.getHomePage = (req, res) => {
+    Puja.findAll({}).then(data => {
+        if (req.session.role === "pandit") {
+            res.render('panditDashboard',{
+                puja: data,
+                isLogin: req.session.isLogin
+            })
+        } else {
+            res.render('index', {
+                puja: data,
+                isLogin: req.session.isLogin
+            })
+        }
+
         console.log(req.session.isLogin)
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err)
         res.send("error")
     })
-    
+
 }
 
-exports.getAboutPage = (req,res)=>{
+exports.getAboutPage = (req, res) => {
     res.send("About page")
 }
 
-exports.getPuja = (req,res)=>{
+exports.getPuja = (req, res) => {
     Puja.findAll({
         where: {
             p_id: req.params.pname
         }
-    }).then(data=>{
-        res.render('details',{
+    }).then(data => {
+        res.render('details', {
             title: data,
             body: details,
             isLogin: req.session.isLogin
         })
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err)
         res.send("error")
     })
